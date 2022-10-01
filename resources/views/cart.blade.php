a717a3046a6d6d1234ec9a5edc06d157dffed59b
brigalang <bryanonielb@gmail.com>
2022-10-01 18:01:44 +0800

    add cart page

---
+++ resources/views/cart.blade.php
@@ -0,0 +1,93 @@
+<x-cart-layout>
+
+    <section class="px-32 grid space-y-4">
+        <div class="bg-white flex justify-between px-10 py-4 shadow-md shadow-stone-200">
+            <div class="flex space-x-2 items-center">
+                <input type="checkbox">
+                <h3>Product</h3>
+            </div>
+            <div class="flex space-x-20">
+                <h3 class="text-gray-400 font-medium">Unit Price</h3>
+                <h3 class="text-gray-400 font-medium">Quantity</h3>
+                <h3 class="text-gray-400 font-medium">Total Price</h3>
+                <h3 class="text-gray-400 font-medium">Actions</h3>
+            </div>
+        </div>
+
+        <div class="bg-white shadow-md shadow-stone-200">
+            <div class="flex space-x-2 px-10 py-4 border-b border-gray-400">
+                <input type="checkbox">
+                <h1>Store Name Here</h1>
+            </div>
+            <div class="flex justify-between px-10 py-4 border-b border-gray-400">
+                <div class="flex justify-start space-x-2">
+                    <input type="checkbox">
+                    <div class="flex items-start space-x-2">
+                        <img src="https://i.pravatar.cc/80" alt="">
+                        <h1>Product Name Here</h1>
+                    </div>
+                </div>
+                <div class="flex items-center space-x-32">
+                    <h3 class="text-gray-400 font-medium">₱70</h3>
+                    <h3 class="text-gray-400 font-medium">2</h3>
+                    <h3 class="text-gray-400 font-medium">₱140</h3>
+                    <button class="text-red-500 font-medium">Delete</button>
+                </div>
+            </div>
+        </div>
+        <div class="bg-white shadow-md shadow-stone-200">
+            <div class="flex space-x-2 px-10 py-4 border-b border-gray-400">
+                <input type="checkbox">
+                <h1>Store Name Here</h1>
+            </div>
+            <div class="flex justify-between px-10 py-4 border-b border-gray-400">
+                <div class="flex justify-start space-x-2">
+                    <input type="checkbox">
+                    <div class="flex items-start space-x-2">
+                        <img src="https://i.pravatar.cc/80" alt="">
+                        <h1>Product Name Here</h1>
+                    </div>
+                </div>
+                <div class="flex items-center space-x-32">
+                    <h3 class="text-gray-400 font-medium">₱70</h3>
+                    <h3 class="text-gray-400 font-medium">2</h3>
+                    <h3 class="text-gray-400 font-medium">₱140</h3>
+                    <button class="text-red-500 font-medium">Delete</button>
+                </div>
+            </div>
+        </div>
+        <div class="bg-white shadow-md shadow-stone-200">
+            <div class="flex space-x-2 px-10 py-4 border-b border-gray-400">
+                <input type="checkbox">
+                <h1>Store Name Here</h1>
+            </div>
+            <div class="flex justify-between px-10 py-4 border-b border-gray-400">
+                <div class="flex justify-start space-x-2">
+                    <input type="checkbox">
+                    <div class="flex items-start space-x-2">
+                        <img src="https://i.pravatar.cc/80" alt="">
+                        <h1>Product Name Here</h1>
+                    </div>
+                </div>
+                <div class="flex items-center space-x-32">
+                    <h3 class="text-gray-400 font-medium">₱70</h3>
+                    <h3 class="text-gray-400 font-medium">2</h3>
+                    <h3 class="text-gray-400 font-medium">₱140</h3>
+                    <button class="text-red-500 font-medium">Delete</button>
+                </div>
+            </div>
+        </div>
+
+        <div class=" bg-white bottom-3 px-10 py-4 shadow-md shadow-stone-200">
+            <div class="flex items-center space-x-2">
+                <h3>Total (0 item):&nbsp;</h3>
+                <h3>₱0&nbsp;</h3>
+                <button class="bg-blue-400 px-5 py-2 text-white hover:bg-blue-500">Checkout</button>
+            </div>
+
+        </div>
+    </section>
+
+
+
+</x-cart-layout>
