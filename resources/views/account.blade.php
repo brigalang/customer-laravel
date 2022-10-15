211798feafe144cbcb905877f2af8b9e49373b34
brigalang <bryanonielb@gmail.com>
2022-10-16 02:03:13 +0800

    refactor code on account page

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,7 +1,7 @@
-<x-head>Reservoir | Account</x-head>
-<x-layout>
-    <div class="flex flex-wrap w-fit justify-center px-10 mt-5 ">
-        <x-edit-profile/>
-        <x-wallet/>
-    </div>
-</x-layout>
+<x-head-customer>Reservoir | Account</x-head>
+    <x-layout>
+        <div class="flex flex-wrap w-fit justify-center px-10 mt-5 ">
+            <x-edit-profile />
+            <x-wallet />
+        </div>
+    </x-layout>
